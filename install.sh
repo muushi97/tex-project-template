939bb397cf68f81ce36ceb67798269751ca637c4
kohei nishimuta <moouushi@gmail.com>
2020-04-24 14:44:43 +0900

    fix makefile to typeset multi-file latex project

--- install.sh
+++ install.sh
@@ -10,8 +10,11 @@ ln -s ${TEMPLATE_DIRECTORY}/.latexmkrc ${PROJECT_DIRECTORY}/.latexmkrc
 ln -s ${TEMPLATE_DIRECTORY}/preambles ${PROJECT_DIRECTORY}/preambles
 ln -s ${TEMPLATE_DIRECTORY}/Makefile ${PROJECT_DIRECTORY}/Makefile
 ln -s ${TEMPLATE_DIRECTORY}/.gitignore ${PROJECT_DIRECTORY}/.gitignore
-#mkdir -p ${PROJECT_DIRECTORY}/images
-cp -rb ./images ${PROJECT_DIRECTORY}/images
-cp -b ./master.ltx ${PROJECT_DIRECTORY}/master.ltx
+mkdir -p ${PROJECT_DIRECTORY}/images
+mkdir -p ${PROJECT_DIRECTORY}/sources
+#cp -rb ./images ${PROJECT_DIRECTORY}/images
+cp -b ./master.ltx ${PROJECT_DIRECTORY}/sources/master.ltx
 
+cd ${PROJECT_DIRECTORY}
+ln -s ./sources/master.ltx ./master.ltx
 
